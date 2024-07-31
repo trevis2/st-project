4a8f480617f0d54359352b9295867f4399451997
Simone Trevisan <simone.trevisan@ibc.srl>
2024-07-31 22:28:54 +0200

    update per sonal projects

--- fizz-buzz-java/src/main/java/it/trevisan/simone/Main.java
+++ fizz-buzz-java/src/main/java/it/trevisan/simone/Main.java
@@ -1,31 +1,111 @@
 package it.trevisan.simone;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        assertThat(fizzBuzz(3)).isEqualTo("1,2,Fizz");
-        assertThat(fizzBuzz(5)).isEqualTo("1,2,Fizz,4,Buzz");
-        assertThat(fizzBuzz(10)).isEqualTo("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz");
-        assertThat(fizzBuzz(15)).isEqualTo("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz");
-        assertThat(fizzBuzz(20))
-                .isEqualTo("1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz");
-        assertThat(fizzBuzz(30)).isEqualTo(
-                "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz");
-        assertThat(fizzBuzz(100)).isEqualTo(
-                "1,2,Fizz,4,Buzz,Fizz,7,8,Fizz,Buzz,11,Fizz,13,14,FizzBuzz,16,17,Fizz,19,Buzz,Fizz,22,23,Fizz,Buzz,26,Fizz,28,29,FizzBuzz,31,32,Fizz,34,Buzz,Fizz,37,38,Fizz,Buzz,41,Fizz,43,44,FizzBuzz,46,47,Fizz,49,Buzz,Fizz,52,53,Fizz,Buzz,56,Fizz,58,59,FizzBuzz,61,62,Fizz,64,Buzz,Fizz,67,68,Fizz,Buzz,71,Fizz,73,74,FizzBuzz,76,77,Fizz,79,Buzz,Fizz,82,83,Fizz,Buzz,86,Fizz,88,89,FizzBuzz,91,92,Fizz,94,Buzz,Fizz,97,98,Fizz,Buzz");
+        Stream<String> stream = Stream.of("uno", "due", "tre", "quattro", "cinque");
+
+        // Invertiamo gli elementi dello stream e li stampiamo
+        ArrayList<String> list = stream.collect(Collectors.toCollection(ArrayList::new));
+        Collections.reverse(list);
+        list.forEach(System.out::println);
+
+        HashMap<Integer, String> configMap = new HashMap<Integer, String>();
+        // configMap.put(2, "DUE");
+        configMap.put(3, "Tre");
+        configMap.put(7, "Sette");
+        configMap.put(11, "Undici");
+        configMap.put(13, "Tredici");
+        configMap.put(17, "Diciassette");
+        configMap.put(19, "Diciannove");
+        configMap.put(23, "Ventitr");
+
+        FizzBuzz item = new FizzBuzz(configMap);
+        // verify(item.generate2(2), configMap, 2);
+        // verify(item.generate2(3), configMap, 3);
+        // verify(item.generate2(5), configMap, 5);
+        // verify(item.generate2(500), configMap, 500);
+
+        int test = 100;
+        long start = System.currentTimeMillis();
+        item.generate(test);
+        System.out.println("generate:" + (System.currentTimeMillis() - start));
+        start = System.currentTimeMillis();
+        item.generate2(test);
+        System.out.println("generate2:" + (System.currentTimeMillis() - start));
+        // scrivere un generatore di stringhe che generi una sequenza di caratteri lunga
+        // n (da input) separati da virgola secondo le seguenti regole:
+        // nelle posizioni multiple di 3 il numero è sostituito da "Tre"
+        // nelle posizioni multiple di 5 il numero è sostituito da "Cinque"
+        // nelle posizioni multiple sia di 3 che 5 il numero è sostituito da "TreCinque"
+        // nelle restanti ci deve essere il numero della posizione
+        // esempi
+        // generate(3) > "1,2,Tre"
+        // generate(5) > "1,2,Tre,4,Cinque"
+        // generate(15) > "1,2,Tre,4,Cinque,Tre,7,8,Tre,Cinque,11,Tre,13,14,TreCinque"
+
+        // scrivere un test per generate(50000)
     }
 
-    public static String fizzBuzz(int n) {
+    static void verify(String stringDaVerify, HashMap<Integer, String> configMap, int numeroElementi) {
+        String[] array = stringDaVerify.split(",");
+        try {
+            assertThat(array.length == numeroElementi)
+                    .as("lunghezza array errata:" + array.length + ", expected:" + numeroElementi).isTrue();
+
+            configMap.keySet().stream().forEach(prime -> {
+                long conto = Arrays.stream(array).filter(item -> item.contains(configMap.get(prime))).count();
+                assertThat((numeroElementi / prime) == conto)
+                        .as("la divisione di " + prime + ":" + numeroElementi / prime + ", non torna con il conto:"
+                                + conto)
+                        .isTrue();
+            });
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
+
+class FizzBuzz {
+    HashMap<Integer, String> configMap;
+    ArrayList<Integer> arl;
+
+    FizzBuzz(HashMap<Integer, String> config) {
+        this.configMap = config;
+        Set<Integer> sets = configMap.keySet();
+        ArrayList<Integer> al = new ArrayList<>();
+        al.addAll(sets);
+        setArl(al);
+    }
+
+    private void setArl(ArrayList<Integer> al) {
+        this.arl = al;
+    }
+
+    public String generate(int n) {
         String compositeString = "";
         for (int i = 1; i < n + 1; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
-                compositeString += "FizzBuzz";
-            } else if (i % 3 == 0) {
-                compositeString += "Fizz";
-            } else if (i % 5 == 0) {
-                compositeString += "Buzz";
+            boolean found = false;
 
+            for (int j : arl) {
+                if (i % j == 0) {
+                    compositeString += configMap.get(j);
+                    found = true;
+                }
+            }
+            if (found) {
+                // non fare niente
             } else {
                 compositeString += i;
             }
@@ -34,5 +114,16 @@ public class Main {
             }
         }
         return compositeString;
+    }
+
+    public String generate2(Integer n) {
+        return IntStream
+                .rangeClosed(1, n)
+                .mapToObj(nvalue -> {
+                    String resp = configMap.keySet().stream().filter(k -> nvalue % k == 0).map(configMap::get)
+                            .collect(Collectors.joining());
+                    return resp.isEmpty() ? Integer.toString(nvalue) : resp;
+                })
+                .collect(Collectors.joining(","));
     }
 }
