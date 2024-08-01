2ebe6127b30858f69430b31e6bb50e2fb8617b6f
Simone Trevisan <simone.trevisan@ibc.srl>
2024-08-01 17:40:05 +0200

    rest api and vercel settings

--- app-personal-backend/app.js
+++ app-personal-backend/app.js
@@ -8,8 +8,8 @@ const port = 3003
 var whitelist = ['http://example1.com', 'http://example2.com', 'http://localhost']
 var corsOptions = {
     origin: (origin, callback) => {
-        console.log('origin', origin)
-        console.log('whitelist', whitelist)
+        // console.log('origin', origin)
+        // console.log('whitelist', whitelist)
         if (whitelist.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
         }
@@ -28,10 +28,33 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/user', async (req, res) => {
+app.post('/item', async (req, res) => {
     const { description } = req.body;
-    const newTodo = await pool.query("INSERT INTO TODO (description) VALUES($1)", [description])
-    res.json(newTodo);
+    const item = await pool.query("INSERT INTO TODO (description) VALUES($1) RETURNING *", [description])
+    res.json(item);
+})
+
+app.delete('/item/:id', async (req, res) => {
+    const { id } = req.params;
+    const item = await pool.query("DELETE FROM TODO WHERE ID=$1 RETURNING *", [id])
+    res.json(item.rows);
+})
+
+app.get('/item/:id', async (req, res) => {
+    const { id } = req.params;
+    const item = await pool.query("SELECT * FROM TODO WHERE ID=$1", [id])
+    res.json(item.rows);
+})
+
+app.put('/item', async (req, res) => {
+    const { id, description } = req.body;
+    const item = await pool.query("UPDATE TODO SET DESCRIPTION=$1 WHERE ID=$2 RETURNING *", [description, id])
+    res.json(item.rows);
+})
+
+app.get('/items', async (req, res) => {
+    const items = await pool.query("SELECT * FROM TODO ORDER BY ID");
+    res.json(items.rows);
 })
 
 
@@ -40,3 +63,5 @@ app.listen(port,
         console.log(`app listening at port ${port}`)
     }
 )
+
+module.exports = app;
